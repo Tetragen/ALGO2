aac1611d3c45997460ef590c0de049a92c05d90b
Nicolas Le Hir <nico@iMac-de-Nicolas.local>
2019-11-09 18:35:35 +0100

    editcompatibility graphs

--- code/compatibility_simple/build_graph.py
+++ code/compatibility_simple/build_graph.py
@@ -1,36 +1,15 @@
 import numpy as np
+import os
 import matplotlib.pyplot as plt
 import math
 
-# x, y = np.random.random(size=(2, 10))
-
-# print(x, y)
-
-# for i in range(0, len(x), 2):
-#     plt.plot(x[i:i + 2], y[i:i + 2], '-')
-
-
-# x = [-1, 0.5, 1, -0.5]
-# y = [0.5,  1, -0.5, -1]
-
 n_data = 20
 
-data_1 = np.random.normal(loc=(2, 0.5), scale=0.1, size=(n_data, 2))
-# print('----')
-# print(data_1)
-# x_1 = data_1[0]
-# y_1 = data_1[1]
-
-data_2 = np.random.normal(loc=(0, 0), scale=0.5, size=(n_data, 2))
-# print('----')
-# print(data_2)
-# x_2 = data_2[0]
-# y_2 = data_2[1]
-
-data = np.concatenate((data_1, data_2))
-# print('----')
-# print(data)
-
+# data_1 = np.random.normal(loc=(2, 0.5), scale=0.1, size=(n_data, 2))
+# data_2 = np.random.normal(loc=(0, 0), scale=0.5, size=(n_data, 2))
+# data = np.concatenate((data_1, data_2))
+# np.save("data",data)
+data=np.load("./data.npy")
 
 x = data[:, 0]
 y = data[:, 1]
@@ -42,18 +21,19 @@ def connectpoints(x, y, p1, p2):
     y1, y2 = y[p1], y[p2]
     plt.plot([x1, x2], [y1, y2], 'k-')
 
-threshold = 3
+threshold = 0.5
 
 for (i, j) in [(i, j) for i in range(1, x.shape[0]) for j in range(1, x.shape[0])]:
         # we look points a and b
-    x_a = x[i]
-    y_a = y[i]
-    x_b = x[j]
-    y_b = y[j]
-    euclidian_distance = math.sqrt((x_a - x_b)**2 + (y_a - y_b)**2)
+    x_i = x[i]
+    y_i = y[i]
+    x_j = x[j]
+    y_j = y[j]
+    euclidian_distance = math.sqrt((x_i - x_j)**2 + (y_i - y_j)**2)
     if euclidian_distance <= threshold:
         connectpoints(x, y, i, j)
 
+
 # plt.axis('equal')
-plt.savefig('clustering_threshold/thres_' + str(threshold) + '.pdf')
+plt.savefig(f"clustering_threshold/thres_{threshold}.pdf")
 plt.close()
